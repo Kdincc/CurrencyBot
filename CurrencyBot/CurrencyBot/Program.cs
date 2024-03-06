0a9b9da70eb845059143e591138b5996d9629775
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-03-06 13:53:52 +0200

    Add BotToken to appsettings

--- CurrencyBot/CurrencyBot/Program.cs
+++ CurrencyBot/CurrencyBot/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace CurrencyBot
+﻿namespace CurrencyBot;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
