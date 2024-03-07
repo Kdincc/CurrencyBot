b8e84d1fe572423cb1e38a44ceca72a389825844
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-03-07 15:00:30 +0200

    Add DependancyInjection

---
+++ CurrencyBot/CurrencyBot/DependancyInjection.cs
@@ -0,0 +1,23 @@
+﻿using CurrencyBot.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Polling;
+
+namespace CurrencyBot
+{
+    public static class DependancyInjection
+    {
+        public static IServiceCollection RegisterService(this IServiceCollection services) 
+        {
+            services.AddTransient<ICurrencyService, CurrencyService>();
+            services.AddTransient<IUpdateHandler, UpdateHandler>();
+            services.AddTransient<IUserResponseParser, UserResponseParser>();
+
+            return services;
+        }
+    }
+}
