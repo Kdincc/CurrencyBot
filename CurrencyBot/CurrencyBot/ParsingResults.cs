c1e5f294a9cb99e917c84adfbf475d422cc2630e
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-03-07 12:18:02 +0200

    Add ParsingResults

---
+++ CurrencyBot/CurrencyBot/ParsingResults.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyBot
+{
+    public class ParsingResults<T>(T result, bool isValid)
+    {
+        private readonly T _result = result;
+        private readonly bool _isValid = isValid;
+
+        public T Result => _result;
+
+        public bool IsValid => _isValid;
+    }
+}
