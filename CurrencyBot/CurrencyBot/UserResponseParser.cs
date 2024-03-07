1e7d0c890ad5e9917c9f7c7e16da49d75e2548e0
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-03-07 15:49:53 +0200

    Implement UserResponseParser

--- CurrencyBot/CurrencyBot/UserResponseParser.cs
+++ CurrencyBot/CurrencyBot/UserResponseParser.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CurrencyBot
@@ -11,7 +12,31 @@ namespace CurrencyBot
     {
         public ParsingResults<ExchangeInfo> ParseExchangeInfo(string response)
         {
-            throw new NotImplementedException();
+            Regex regex = new(@"code: ...\, date: \d\d\.\d\d\.\d\d\d\d");
+            string lowerResponse = response.ToLower();
+
+            if (regex.IsMatch(lowerResponse))
+            {
+                var parametres = lowerResponse.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                var code = GetParametrValue(parametres[0]);
+                var dateString = GetParametrValue(parametres[1]);
+
+                if (DateOnly.TryParse(dateString, out DateOnly date))
+                {
+                    ExchangeInfo info = new(date, code.ToUpper());
+
+                    return new ParsingResults<ExchangeInfo>(info, true);
+                }
+            }
+
+            return new ParsingResults<ExchangeInfo>(new ExchangeInfo(new DateOnly(), string.Empty), false);
+        }
+
+        private string GetParametrValue(string parametr)
+        {
+            int cutIndex = parametr.IndexOf(':') + 1;
+
+            return parametr[cutIndex ..];
         }
     }
 }
