5d475b874fcb087111f435ca9f8e643eccc5e0b5
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-03-07 11:55:58 +0200

    Add CurrencyService

---
+++ CurrencyBot/CurrencyBot/CurrencyService.cs
@@ -0,0 +1,17 @@
+﻿using CurrencyBot.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyBot
+{
+    public class CurrencyService : ICurrencyService
+    {
+        public Task<ExchangeRate> GetExchangeRateAsync(string cuurencyCode, DateOnly date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
