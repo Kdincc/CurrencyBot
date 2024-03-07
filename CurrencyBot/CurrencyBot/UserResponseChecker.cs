3c93e41c2c242a2e034d48d8c56f041b374d8373
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-03-07 12:02:43 +0200

    Add UserResponseChecker

---
+++ CurrencyBot/CurrencyBot/UserResponseChecker.cs
@@ -0,0 +1,17 @@
+﻿using CurrencyBot.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyBot
+{
+    public class UserResponseChecker : IUserResponseChecker
+    {
+        public bool IsUserResponseCorrect(string response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
