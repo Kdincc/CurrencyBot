4ea246bdcf58e035013eb09f6a23bd5f0a447caf
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-03-07 11:54:20 +0200

    Add ICurrencyService

---
+++ CurrencyBot/CurrencyBot/Interfaces/ICurrencyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyBot.Interfaces
+{
+    public interface ICurrencyService
+    {
+        public Task<ExchangeRate> GetExchangeRateAsync(string cuurencyCode, DateOnly date);
+    }
+}
